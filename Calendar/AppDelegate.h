bc1df2483d46224e0ed4b23d306bb0150e254a6b
lxy <lxy@lxydeiMac.local>
2019-03-04 09:59:49 +0800

    测试

---
+++ Calendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calendar
+//
+//  Created by yiqiang on 16/7/12.
+//  Copyright © 2016年 yiqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
